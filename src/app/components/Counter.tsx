5d2fbce2b1d6b32d60e353ad932f96893145af4f
Esty Friedman <ehshpiner@gmail.com>
2024-11-19 12:20:22 +0200

    css

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -12,9 +12,9 @@ const Counter = () => {
 
   return (
     <div>
-      <div className="h-8 w-full bg-blue-400 rounded-full ">
-        <button onClick={() => dispatch(incrementAsync(10))}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <div className="h-8 w-full rounded-full ">
+        <button onClick={() => dispatch(incrementAsync(10))} className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Increment</button>
+        <button onClick={() => dispatch(decrement())} className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Decrement</button>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 'use client'
 
 import Counter from '@/app/components/Counter';
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from '@/store/counterStore';
+import { useSelector } from "react-redux";
+import { RootState } from '@/store/counterStore';
 
 
 export default function Home() {
   const count = useSelector((state: RootState) => state.counter.value);
 
   return (
-   <div className=''>
-          <h2> Count from Home: {count}</h2>
-
+   <div className='flex flex-col justify-center items-center h-screen space-y-8'>
+    <h2> Count : {count}</h2>
     <Counter/>
    </div>
   );
